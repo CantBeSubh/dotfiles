2668dae241f3eff7940644e7303b4900f77aa555
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-04-17 14:04:05 +0530

    mac: nextjs clearing zsh fucntions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -2,3 +2,7 @@ clear_pycache() {
   find . -name "__pycache__" -type d -exec rm -rf {} \;
   find . -name ".pytest_cache" -type d -exec rm -rf {} \;
 }
+
+clear_nextjs(){
+  find . -name ".next" -type d -exec rm -rf {} \;
+}
