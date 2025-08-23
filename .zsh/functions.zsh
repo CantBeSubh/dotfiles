5a574a357e37762eeb13557547c1b48839dc0fe4
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-08-23 20:20:18 +0530

    mac: added yazi

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -6,3 +6,11 @@ clear_pycache() {
 clear_nextjs(){
   find . -name ".next" -type d -exec rm -rf {} \;
 }
+
+yz() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
