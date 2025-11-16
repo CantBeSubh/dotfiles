390334c3a2957be52eb335b5201154079ca49faa
Subhranshu Pati <subhranshupati0412@gmail.com>
2025-11-16 11:12:02 +0530

    mac: lock file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
